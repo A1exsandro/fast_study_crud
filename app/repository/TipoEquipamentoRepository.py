b20ec45bc3fff8dcffaa0c72d2cbd7dfe47fcdb7
A1exsandro <alex.con100trado@gmail.com>
2024-01-04 17:43:49 -0400

    read update and delete tipo_equipamento

--- app/repository/TipoEquipamentoRepository.py
+++ app/repository/TipoEquipamentoRepository.py
@@ -16,3 +16,127 @@ class TipoEquipamentoRepository:
         nome_upcase = create_form.nome.upper()
         db.add(TipoEquipamento(nome=nome_upcase))
         await commit_rollback()
+
+    # GET BY ID
+    @staticmethod
+    async def get_by_id(tipo_equipamento_id: int):
+        """ retrieve data by id """
+        query = select(TipoEquipamento).where(TipoEquipamento.id == tipo_equipamento_id)
+        return (await db.execute(query)).scalar_one_or_none()
+    
+    # UPDATE
+    @staticmethod
+    async def update(tipo_equipamento_id: int, update_form: TipoEquipamentoCreate):
+        """ update data """
+
+        query = update(TipoEquipamento) \
+            .where(TipoEquipamento.id == tipo_equipamento_id) \
+            .values(**update_form.dict()) \
+            .execution_options(synchronize_session="fetch")
+
+        await db.execute(query)
+        await commit_rollback()
+
+    # DELETE
+    @staticmethod
+    async def delete(tipo_equipamento_id: int):
+        """ delete data by id """
+
+        query = delete(TipoEquipamento).where(TipoEquipamento.id == tipo_equipamento_id)
+        await db.execute(query)
+        await commit_rollback()
+
+    # READ
+    @staticmethod
+    async def get_all(
+            page: int = 1,
+            limit: int = 10,
+            columns: str = None,
+            sort: str = None,
+            filter: str = None
+    ):
+        query = select(from_obj=TipoEquipamento, columns="*")
+
+        # select columns dynamically
+        if columns is not None and columns != "all":
+            # we need column format data like this --> [column(id),column(name),column(sex)...]
+
+            query = select(from_obj=TipoEquipamento, columns=convert_columns(columns))
+
+        # select filter dynamically
+        if filter is not None and filter != "null":
+            # we need filter format data like this  --> {'name': 'an','country':'an'}
+
+            # convert string to dict format
+            criteria = dict(x.split("*") for x in filter.split('-'))
+
+            criteria_list = []
+
+            # check every key in dict. are there any table attributes that are the same as the dict key ?
+
+            for attr, value in criteria.items():
+                _attr = getattr(TipoEquipamento, attr)
+
+                # filter format
+                search = "%{}%".format(value)
+
+                # criteria list
+                criteria_list.append(_attr.like(search))
+
+            query = query.filter(or_(*criteria_list))
+
+        # select sort dynamically
+        if sort is not None and sort != "null":
+            # we need sort format data like this --> ['id','name']
+            query = query.order_by(text(convert_sort(sort)))
+
+        # count query
+        count_query = select(func.count(1)).select_from(query)
+
+        offset_page = page - 1
+        # pagination
+        query = (query.offset(offset_page * limit).limit(limit))
+
+        # total record
+        total_record = (await db.execute(count_query)).scalar() or 0
+
+        # total page
+        total_page = math.ceil(total_record / limit)
+
+        # result
+        result = (await db.execute(query)).fetchall()
+
+        return PageResponse(
+            page_number=page,
+            page_size=limit,
+            total_pages=total_page,
+            total_record=total_record,
+            content=result
+        )
+
+
+def convert_sort(sort):
+    """
+    # separate string using split('-')
+    split_sort = sort.split('-')
+    # join to list with ','
+    new_sort = ','.join(split_sort)
+    """
+    return ','.join(sort.split('-'))
+
+
+def convert_columns(columns):
+    """
+    # seperate string using split ('-')
+    new_columns = columns.split('-')
+
+    # add to list with column format
+    column_list = []
+    for data in new_columns:
+        column_list.append(data)
+
+    # we use lambda function to make code simple
+
+    """
+
+    return list(map(lambda x: column(x), columns.split('-')))
--- app/router/tipo_equipamento.py
+++ app/router/tipo_equipamento.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
 
 
 from app.repository.TipoEquipamentoRepository import TipoEquipamentoRepository
@@ -18,3 +18,40 @@ async def create_tipo_equipamento(
     await TipoEquipamentoRepository.create(create_form)
     return ResponseSchema(detail="Successfully created data !")
 
+# GET BY ID
+@router.get("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
+async def get_tipo_equipamento_by_id(
+        tipo_equipamento_id: int = Path(..., alias="id")
+):
+    result = await TipoEquipamentoRepository.get_by_id(tipo_equipamento_id)
+    return ResponseSchema(detail="Successfully fetch person data by id !", result=result)
+
+# UPDATE
+@router.patch("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
+async def update_tipo_equipamento(
+        tipo_equipamento_id: int = Path(..., alias="id"),
+        *,
+        update_form: TipoEquipamentoCreate
+):
+    await TipoEquipamentoRepository.update(tipo_equipamento_id, update_form)
+    return ResponseSchema(detail="Successfully updated data !")
+
+# DELETE
+@router.delete("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
+async def delete_tipo_equipamento(
+        tipo_equipamento_id: int = Path(..., alias="id"),
+):
+    await TipoEquipamentoRepository.delete(tipo_equipamento_id)
+    return ResponseSchema(detail="Successfully deleted data !")
+
+# READ
+@router.get("", response_model=ResponseSchema, response_model_exclude_none=True)
+async def get_all_tipo_equipamento(
+        page: int = 1,
+        limit: int = 10,
+        columns: str = Query(None, alias="columns"),
+        sort: str = Query(None, alias="sort"),
+        filter: str = Query(None, alias="filter"),
+):
+    result = await TipoEquipamentoRepository.get_all(page, limit, columns, sort, filter)
+    return ResponseSchema(detail="Successfully fetch person data by id !", result=result)
